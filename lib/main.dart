83698c35d6c17841f5d635cf49dcdbb973926f09
snxz21 <snxz21@mail.ru>
2021-06-13 19:28:40 +0500

    add web setting firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:html';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:ui' as ui;
 
 void main() {
   runApp(MyApp());
@@ -11,7 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AUIS SafeApp',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -30,19 +35,40 @@ class WebViewExampleState extends State<WebViewExample> {
   void initState() {
     super.initState();
     // Enable hybrid composition.
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    // if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    //ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+        'hello-world-html',
+        (int viewId) => IFrameElement()
+          ..width = '640'
+          ..height = '360'
+          ..src = "https://docs.google.com/forms/d/e/1FAIpQLSfRULF_wt3AfwrhkB0wivuRCdcbIYOkl7ETidQFX6SI1J3O0w/viewform"
+          ..style.border = 'none');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: WebView(
-        gestureNavigationEnabled: true,
-        javascriptMode: JavascriptMode.unrestricted,
-        initialUrl:
-            'https://docs.google.com/forms/d/e/1FAIpQLSfRULF_wt3AfwrhkB0wivuRCdcbIYOkl7ETidQFX6SI1J3O0w/viewform',
-      ),
+      // appBar: AppBar(),
+      body: kIsWeb
+          ? Container(
+              // width: MediaQuery.of(context).size.width * 0.5,
+              // height: 300,
+              child: Directionality(
+                textDirection: TextDirection.ltr,
+                child: Container(
+                  // width: 640,
+                  // height: 360,
+                  child: HtmlElementView(viewType: 'hello-world-html'),
+                ),
+              ),
+            )
+          : WebView(
+              gestureNavigationEnabled: true,
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl:
+                  'https://docs.google.com/forms/d/e/1FAIpQLSfRULF_wt3AfwrhkB0wivuRCdcbIYOkl7ETidQFX6SI1J3O0w/viewform',
+            ),
     );
   }
 }
